cbf6dc697c0c68bb374726b670c754bac0cc06f2
juanquinta <quintanillajuan916@gmail.com>
2026-09-09 09:19:45 -0500

    Implement delete function for T_MAT_EVA
    Added a delete function to remove entries from T_MAT_EVA based on UUID.

--- matevaService.py
+++ matevaService.py
@@ -9,3 +9,19 @@ def show():
     data = [ Mateva(x[0], x[1], x[2])for x in data]
     c.close()
     return data
+
+
+def delete(uuid):
+        c = current_app.mysql.connection.cursor()
+        sql = """
+            DELETE FROM T_MAT_EVA WHERE APR_UUID = %s"""
+        c.execute(sql,[ uuid ])
+        c.connection.commit()
+        if c.rowcount > 0 :
+             codigo = 200
+        else:
+             codigp = 404
+
+
+        c.close()
+        return codigo
