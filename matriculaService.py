4d93557483addd0e18c2d27e5c3568eef4d954df
juanquinta <quintanillajuan916@gmail.com>
2026-09-09 09:20:40 -0500

    Update matriculaService.py

--- matriculaService.py
+++ matriculaService.py
@@ -9,3 +9,19 @@ def show():
         data = [ Matricula(x[0], x[1], x[2])for x in data]
         c.close()
         return ""
+
+
+def delete(uuid):
+        c = current_app.mysql.connection.cursor()
+        sql = """
+            DELETE FROM T_MATRICULA WHERE APR_UUID = %s"""
+        c.execute(sql,[ uuid ])
+        c.connection.commit()
+        if c.rowcount > 0 :
+             codigo = 200
+        else:
+             codigp = 404
+
+
+        c.close()
+        return codigo
