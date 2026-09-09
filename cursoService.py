a66e3300c3db79225c03d3eb65750c4562fbce06
juanquinta <quintanillajuan916@gmail.com>
2026-09-09 09:14:01 -0500

    Implement delete function for course removal
    Added delete function to remove a course by UUID.

--- cursoService.py
+++ cursoService.py
@@ -8,3 +8,19 @@ def show():
     data = [ Curso(x[0], x[1], x[2])for x in data]
     c.close()
     return data
+
+def delete(uuid):
+        c = current_app.mysql.connection.cursor()
+        sql = """
+            DELETE FROM T_CURSO WHERE APR_UUID = %s"""
+        c.execute(sql,[ uuid ])
+        c.connection.commit()
+        if c.rowcount > 0 :
+             codigo = 200
+        else:
+             codigp = 404
+
+
+        c.close()
+        return codigo
+
