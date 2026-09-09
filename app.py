eff173bb6331b2447c73d04e2baeea4e5d54a7c0
key202006 <keitlynrivero213@gmail.com>
2026-09-09 10:04:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from Config import Config
+from dotenv import load_dotenv
+from Routes import loadRoutes
+
+load_dotenv()
 
 app = Flask(__name__)
 # blueprint
 app.config.from_object(Config) 
 mysql = MySQL(app)
 
-app.run(debug=True, port=5000, host="0.0.0.0")
+app.mysql = mysql
+loadRoutes(app)
+
+if __name__ == '__main__':
+  app.run(debug=True, port=5000, host="0.0.0.0")
