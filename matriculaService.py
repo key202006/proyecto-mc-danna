15b0cdf631f872e6132b0701e46b72dfc5b94676
juanquinta <quintanillajuan916@gmail.com>
2026-09-09 09:25:24 -0500

    Update matriculaService.py

--- matriculaService.py
+++ matriculaService.py
@@ -2,13 +2,13 @@ from flask import current_app
 from Model.matricula import Matricula
 
 def show():
-        sql = "SELECT * FROM T_MATRICULA"
-        c  = current_app.mysql.connection.cursor()
-        c.execute(sql)
-        data = c.fetchall()
-        data = [ Matricula(x[0], x[1], x[2])for x in data]
-        c.close()
-        return data
+    sql = "SELECT * FROM T_MATRICULA"
+    c  = current_app.mysql.connection.cursor()
+    c.execute(sql)
+    data = c.fetchall()
+    data = [ Matricula(x[0], x[1], x[2])for x in data]
+    c.close()
+    return data
 
 
 def delete(uuid):
