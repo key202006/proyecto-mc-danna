83c9f7206b59ad4535da8c5bfd8d5cb11f5becd7
juanquinta <quintanillajuan916@gmail.com>
2026-09-09 09:07:33 -0500

    Implement delete function for apprentice records
    Add delete function to remove an apprentice by UUID.

--- aprendicesService.py
+++ aprendicesService.py
@@ -11,3 +11,19 @@ class aprendizService:
         data = [ Aprendices(x[0], x[1], x[2])for x in data]
         c.close()
         return ""
+
+     def delete(uuid):
+        c = current_app.mysql.connection.cursor()
+        sql = """
+            DELETE FROM T_APRENDIZ WHERE APR_UUID = %s"""
+        c.execute(sql,[ uuid ])
+        c.connection.commit()
+        if c.rowcount > 0 :
+             codigo = 200
+        else:
+             codigp = 404
+
+
+        c.close()
+        return codigo
+
