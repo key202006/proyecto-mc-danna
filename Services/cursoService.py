7e7321365fc8237bc1ba8d41837bec3944cdbf14
juanquinta <quintanillajuan916@gmail.com>
2026-09-23 11:03:21 -0500

    Add cursoService with show and delete functionality
    Implement cursoService with show and delete methods.

---
+++ Services/cursoService.py
@@ -0,0 +1,29 @@
+from flask import current_app
+from Models.Curso import Curso
+import uuid
+
+class cursoService :
+      
+    def show():
+        sql = "SELECT * FROM T_CURSO"
+        c = current_app.mysql.connection.cursor()
+        c.execute(sql)
+        data = c.fetchall()
+        data = [ Curso(x[0], x[1], x[2])for x in data]
+        c.close()
+        return data
+
+    def delete(uuid):
+            c = current_app.mysql.connection.cursor()
+            sql = """
+                DELETE FROM T_CURSO WHERE APR_UUID = %s"""
+            c.execute(sql,[ uuid ])
+            c.connection.commit()
+            if c.rowcount > 0 :
+                codigo = 200
+            else:
+                codigp = 404
+
+
+            c.close()
+            return codigo
