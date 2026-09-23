74d645f7022be432779b1745b5a7124ddae9ade6
juanquinta <quintanillajuan916@gmail.com>
2026-09-23 11:00:46 -0500

    Add CRUD methods to aprendizService
    Implemented add and delete methods for aprendizService to handle CRUD operations, including UUID generation and database interactions.

--- Services/aprendizService.py
+++ Services/aprendizService.py
@@ -1,14 +1,35 @@
 from flask import current_app
 from Models.Aprendiz import Aprendiz
+import uuid
 
 class aprendizService:
     # opereraciones CRUD
     # CREATE, READ, UPDATE, DELETE
-    def add():
-        pass
+    def add(data):
+        uuid_apr = uuid.uuid4() # genera un uuid
+        c  = current_app.mysql.connection.cursor()
+        sql = """INSERT INTO T_APRENDIZ (APR_UUID, APR_FECHA_NAC, APR_PER_ID) VALUES  
+        (%s, %s, %s)""" #consulta parametrizada
+        c.execute(sql, (uuid_apr, data['APR_FECHA_NAC'], data['APR_PER_ID']))
+        c.connection.commit() # guarda los cambios
+        id = c.lastrowid # devuelve el id del ultimo registro insertado
+        c.close()
+        respuesta = {"id":id, "APR_UUID": uuid_apr, "APR_FECHA_NAC" : data["APR_FECHA_NAC"], 
+                     "APR_PER_ID" : data["APR_PER_ID"]}
+        return respuesta
 
-    def delete():
-        pass
+    def delete(uuid):
+        c  = current_app.mysql.connection.cursor()
+        sql = f""" delete from T_APRENDIZ where APR_UUID = %s"""
+        c.execute(sql, [uuid])
+        c.connection.commit() # guarda los cambios
+        if c.rowcount > 0 :
+           codigo= 200 
+        else:
+           codigo = 404
+            
+        c.close()
+        return codigo
 
     def update():
         pass
@@ -18,6 +39,7 @@ class aprendizService:
         c  = current_app.mysql.connection.cursor()
         c.execute(sql)
         data = c.fetchall()
-        print(data)
-        # c.close()
-        return ""
+        data = [ Aprendiz(x[0], x[1], x[2], x[3]) for x in data]
+        c.close()
+        return data
+# relacionado todo lo q esta en la base de datos con la clase aprendiz
