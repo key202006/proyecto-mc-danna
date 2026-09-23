6dc46174a883c6fbdb6f0d02c889d9cca7218943
juanquinta <quintanillajuan916@gmail.com>
2026-09-23 11:04:47 -0500

    Add evaluacion service with show and delete methods

---
+++ Services/evaluacionServices.py
@@ -0,0 +1,30 @@
+from flask import current_app
+from Models.evaluacion import Evaluacion
+import uuid
+
+class evaluacion:
+
+    def show():
+        sql = "SELECT * FROM T_EVALUACION"
+        c = current_app.mysql.connection.cursor()
+        c.execute(sql)
+        data = c.fetchall()
+        data = [ Evaluacion(x[0], x[1], x[2])for x in data]
+        print(data)
+        return data
+
+
+    def delete(uuid):
+            c = current_app.mysql.connection.cursor()
+            sql = """
+                DELETE FROM T_EVALUACION WHERE APR_UUID = %s"""
+            c.execute(sql,[ uuid ])
+            c.connection.commit()
+            if c.rowcount > 0 :
+                codigo = 200
+            else:
+                codigp = 404
+
+
+            c.close()
+            return codigo
